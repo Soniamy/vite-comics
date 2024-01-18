9bb1690febd8528c675a6366c77f3ab4ce06085e
Soniamy <marianna.marzullo@gmail.com>
2024-01-18 17:43:53 +0100

    creazione prima parte del Main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,26 @@
 <script>
 export default {
-    data() {
-        return {
-
-        };
-    },
-    methods: {
-
-    }
-}
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
 
 <template>
-    <main>
-        MAIN
-    </main>
+  <main>
+    <div class="background container">
+      <h1>Content goes here</h1>
+    </div>
+  </main>
 </template>
 
 <style lang="scss" scoped>
+.background {
+  background-color: rgb(28, 28, 28);
+  height: 250px;
+}
+h1 {
+  margin-top: 80px;
+}
 </style>
